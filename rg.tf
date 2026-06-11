04d40356a47379aaa92c5e595013dd85c8c177aa
abhishekkumary213-code <abhishekkumary213@gmail.com>
2026-06-11 14:58:00 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,3 +2,15 @@ resource "azurerm_resource_group" "rg1" {
 name = "rg1"
 location = "west us"
 {
+resource "azurerm_resource_group" "rg2" {
+name = "rg2"
+location = "west us"
+{
+resource "azurerm_resource_group" "rg3" {
+name = "rg3"
+location = "west us"
+{
+resource "azurerm_resource_group" "rg4" {
+name = "rg14"
+location = "west us"
+{
